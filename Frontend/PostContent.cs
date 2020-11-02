d6b4b90444de76fd17049a3a3b4a41366cbc60f7
Julian Hüppauff <julian@hueppauff.info>
2020-11-02 21:33:47 +0100

    added get metadata

--- Frontend/PostContent.cs
+++ Frontend/PostContent.cs
@@ -7,13 +7,14 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos.Table;
 
 namespace Frontend
 {
     public static class PostContent
     {
         [FunctionName("GetPost")]
-        public static IActionResult Run(
+        public static IActionResult GetPost(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetPost/{slug}")] HttpRequest req,
             [Blob("$web/{slug}.html", FileAccess.Read, Connection = "AzureStorageConnection")] string content,
             ILogger log)
@@ -21,6 +22,18 @@ namespace Frontend
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             return new OkObjectResult(content);
+        }
+
+        [FunctionName("GetPostMetadata")]
+        public static IActionResult GetPostMetadata(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetPostMetadata/{slug}")] HttpRequest req,
+            [Blob("$web/{slug}.html", FileAccess.Read, Connection = "AzureStorageConnection")] string content,
+            [Table("metadata", "{slug}", "{slug}", Connection = "AzureStorageConnection")] PostMetadata data,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(data);
         }
     }
 }
---
+++ Frontend/PostMetadata.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend
+{
+    public class PostMetadata : TableEntity
+    {
+        public PostMetadata()
+        {
+        }
+
+        public PostMetadata(string Title)
+        {
+            PartitionKey = Title;
+            RowKey = Title;
+        }
+
+        public string Title { get; set; }
+
+        public string Published { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
