272f82dfdd4f2053a99c442dae41b06a5fdb06aa
Julian Hüppauff <julian@hueppauff.info>
2020-11-08 11:12:56 +0100

    Updated authentication

--- Engine/StaticPageFunction.cs
+++ Engine/StaticPageFunction.cs
@@ -19,12 +19,11 @@ namespace Engine
 
         [FunctionName(nameof(GetAddPostPage))]
         public async Task<IActionResult> GetAddPostPage(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "static/GetAddPostPage")] HttpRequest req, ExecutionContext context,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "static/GetAddPostPage")] HttpRequest req, ExecutionContext context,
             ILogger log)
         {
-            if (!User.Identity.IsAuthenticated)
+            if (!IsEasyAuthEnabled || !User.Identity.IsAuthenticated)
             {
-                log.LogInformation($"Forbidden - Easy Auth : {IsEasyAuthEnabled}, Authenticated : {User.Identity.IsAuthenticated}");
                 return Forbid();
             }
 
