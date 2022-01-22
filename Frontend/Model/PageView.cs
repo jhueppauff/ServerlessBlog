6cb52504ab8274efc8b34771b34869da08916ca3
Julian Hüppauff <julian@hueppauff.com>
2022-01-22 12:45:50 +0100

    added metric tracking

---
+++ Frontend/Model/PageView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace ServerlessBlog.Frontend.Model
+{
+    public class PageView : TableEntity
+    {
+        public PageView()
+        {
+        }
+
+        public PageView(string slug)
+        {
+            PartitionKey = slug;
+            RowKey = DateTime.UtcNow.ToString("yyyyMMdd:mm:ss");
+        }
+    }
+}
