3442378856e43dd28f49e18b3c9ee53401517967
Julian Hüppauff <julian@hueppauff.com>
2022-08-07 22:16:00 +0200

    fixed method

--- Engine/ImageProcessor.cs
+++ Engine/ImageProcessor.cs
@@ -18,7 +18,7 @@ namespace Engine
     {
 
         [FunctionName(nameof(UploadImage))]
-        public static async Task<IActionResult> UploadImage([HttpTrigger(AuthorizationLevel.Anonymous, methods: "Post", Route = "Image/Upload/{extension}")] HttpRequest request, string extension,
+        public static async Task<IActionResult> UploadImage([HttpTrigger(AuthorizationLevel.Anonymous, methods: "Put", Route = "Image/Upload/{extension}")] HttpRequest request, string extension,
         [Blob("public", FileAccess.Write, Connection = "AzureStorageConnection")] BlobContainerClient container, ILogger log)
         {
             log.LogInformation("Triggered Upload Function");
