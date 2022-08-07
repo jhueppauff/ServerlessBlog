f4c4e739884c0371c5f0ca2a224734e0229fdbbe
Julian Hüppauff <julian@hueppauff.com>
2022-08-07 17:06:27 +0200

    added blob retrival

--- Engine/ImageProcessor.cs
+++ Engine/ImageProcessor.cs
@@ -5,9 +5,12 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Engine.Model;
+using System.Collections.Generic;
 
 namespace Engine
 {
@@ -26,6 +29,31 @@ namespace Engine
 
             using Stream stream = request.Form.Files[0].OpenReadStream();
             await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = contentType });
+        }
+
+        [FunctionName(nameof(GetBlobs))]
+        public static async Task<IActionResult> GetBlobs([HttpTrigger(AuthorizationLevel.Anonymous, Route = "Image")] HttpRequest request,
+        [Blob("public", FileAccess.Write, Connection = "AzureStorageConnection")] BlobContainerClient container, ILogger log)
+        {
+            log.LogInformation("Triggered GetBlobs Function");
+            List<Blob> blobs = new();
+
+            var resultSegment = container.GetBlobsAsync()
+            .AsPages(default, 50);
+
+            await foreach (Azure.Page<BlobItem> blobPage in resultSegment)
+            {
+                foreach (BlobItem blob in blobPage.Values)
+                {
+                    blobs.Add(new Blob 
+                    { 
+                        Name = blob.Name, 
+                        Url = $"{ container.Uri}/{blob.Name}"
+                    });
+                }
+            }
+
+            return new OkObjectResult(blobs);
         }
     }
 }
---
+++ Engine/Model/Blob.cs
@@ -0,0 +1,9 @@
+namespace Engine.Model
+{
+    public class Blob
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+    }
+}
