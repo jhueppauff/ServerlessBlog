498f191015f9a0e9043f3fe02a0e6559f6cc7308
Julian Hüppauff <julian@hueppauff.com>
2022-09-12 22:45:28 +0200

    added metric trigger

---
+++ Engine/MetricTrigger.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Data.Tables;
+using Azure.WebJobs.Extensions.HttpApi;
+using ServerlessBlog.Engine.Model;
+using Azure;
+using Newtonsoft.Json.Linq;
+
+namespace Engine
+{
+    public class MetricTrigger : HttpFunctionBase
+    {
+        private readonly TableClient tableClient;
+
+        public MetricTrigger(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+            this.tableClient = new TableClient(Environment.GetEnvironmentVariable("CosmosDBConnection"), "metrics");
+        }
+
+        [FunctionName(nameof(GetPageViews))]
+        public async Task<IActionResult> GetPageViews(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "metric/{slug}")] HttpRequest request, string slug,
+            ILogger log)
+        {
+            log.LogInformation($"Function {nameof(GetPageViews)} was triggered");
+
+            AsyncPageable<TableEntity> queryResultsMaxPerPage = tableClient.QueryAsync<TableEntity>(filter: $"PartitionKey eq '{slug}'", maxPerPage: 500);
+
+            int views = 0;
+
+            await foreach (Page<TableEntity> page in queryResultsMaxPerPage.AsPages())
+            {
+                foreach (TableEntity qEntity in page.Values)
+                {
+                    views++;
+                }
+            }
+
+            var response = new JObject
+            {
+                { "Views", views },
+                { "Slug", slug }
+            };
+
+            return new OkObjectResult(response);
+        }
+    }
+}
