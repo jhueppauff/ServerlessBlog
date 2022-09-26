6f99bac434fb5705cbff1a212be964b51e16be1c
Julian Hüppauff <julian@hueppauff.com>
2022-09-27 00:06:01 +0200

    Updated Chart

--- EditorNG/Program.cs
+++ EditorNG/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace EditorNG
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
